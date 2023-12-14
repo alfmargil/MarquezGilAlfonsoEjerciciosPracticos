2c804329ebe20229c3409fd0269c59511d34a57e
alfmargil <alfmargil@gmail.com>
2023-12-14 18:10:28 +0100

    clase 3 ejercicio 1 empezado

---
+++ EjerciciosSemana1/Clase3/Ejercicio1/src/Electrodomestico.java
@@ -0,0 +1,20 @@
+public class Electrodomestico {
+
+    int cod;
+    String marca;
+    String modelo;
+    double consumo;
+    String color;
+
+
+    public Electrodomestico(int cod, String marca, String modelo, double consumo, String color) {
+        this.cod = cod;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.consumo = consumo;
+        this.color = color;
+    }
+
+    public Electrodomestico() {
+    }
+}
